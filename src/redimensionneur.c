a66fcfda6bd7970cdcf05db4d39dd1aaa093ca8b
setr <setr@localhost.localdomain>
2023-02-23 20:50:49 -0500

    termine le squelette du redimensionneur

--- src/redimensionneur.c
+++ src/redimensionneur.c
@@ -22,5 +22,55 @@ int main(int argc, char* argv[]){
     // mémoire partagée et envoyer le résultat sur une autre zone mémoire partagée.
     // N'oubliez pas de respecter la syntaxe de la ligne de commande présentée dans l'énoncé.
 
+    // Obtenir les clés pour les zones de mémoire partagée
+    int opt;
+    int largeur_sortie = 0;
+    int hauteur_sortie = 0;
+    int rMode = 0;
+    while ((opt = getopt(argc, argv, "w:h:r:")) != -1) {
+        switch (opt) {
+            case 'w':
+                largeur_sortie = atoi(optarg);
+                break;
+            case 'h':
+                hauteur_sortie = atoi(optarg);
+                break;
+            case 'r':
+                rMode = atoi(optarg);
+                break;
+            default:
+                perror("Usage: %s -w <largeur> -h <hauteur> -r <rMode> /chemin/vers/mem_entree /chemin/vers/mem_sortie");
+                return -1;
+        }
+    }
+    if (optind + 2 != argc) {
+        perror("Usage: %s -w <largeur> -h <hauteur> -r <rMode> /chemin/vers/mem_entree /chemin/vers/mem_sortie");
+        return -1;
+    }
+    char *input_mem, *output_mem;
+    input_mem = argv[optind];
+    output_mem = argv[optind + 1];
+
+/*
+    initMemoirePartageeLecteur et initMemoirePartageeEcrivain
+
+    lire une image depuis une zone mémoire partagée
+
+    switch (rMode) {
+        case 0:
+            ResizeGrid rg = resizeNearestNeighborInit();
+            resizeNearestNeighbor();
+            écrire la nouvelle image dans la zone mémoire partagée
+            break;
+        case 1:
+            ResizeGrid rg = resizeBilinearInit();
+            resizeBilinear();
+            écrire la nouvelle image dans la zone mémoire partagée
+            break;
+        default:
+            perror("L'argument r doit être 0 ou 1.");
+    }
+*/
+
     return 0;
 }
